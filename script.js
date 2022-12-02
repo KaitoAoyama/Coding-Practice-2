7f810781d303a57e27548d68b896cd7fe31cb2b6
Kaijie Sun <117415896+KaitoAoyama@users.noreply.github.com>
2022-12-02 02:52:56 +0000

    Added comments

--- script.js
+++ script.js
@@ -1,26 +1,29 @@
-// You can create custom variables here
+// variables
 let r ;
 let g ;
 let b ;
 
 function setup() {
+//creating canvas
   createCanvas(500,600);
-  //below is anther example that creates a canvas the heigth and width of the browser window
-  //createCanvas(windowWidth, windowHeight);
+
+//setting the rgb variable to random numbers
   r = random(256);
   g = random(256);
   b = random(256);
-  //setting the ba
+  
+  //setting the babkground
   background('lightblue');
 }
 
-
+//draw circle and fills the color
 function draw() {
   fill(r,g,b);
   // fill(10);
   circle(250, 300, 250); 
 }
 
+//mouse click changes the rgb of the fill
 function mouseClicked() {
   r = random(256);
   g = random(256);
